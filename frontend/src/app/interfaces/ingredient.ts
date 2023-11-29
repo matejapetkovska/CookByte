7c7a0597d4cc3cfdfc35a5b7f0565991a19d827d
matejapetkovska <matejapetkovska2@gmail.com>
2023-11-29 22:47:02 +0100

    Recipe details component

---
+++ frontend/src/app/interfaces/ingredient.ts
@@ -0,0 +1,7 @@
+import {Recipe} from "./recipe";
+
+export interface Ingredient {
+  id: number,
+  name: string,
+  recipe: Recipe
+}
---
+++ frontend/src/app/services/ingredients.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Ingredient} from "../interfaces/ingredient";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientsService {
+
+  ingredient: Ingredient | undefined
+
+  url = 'http://localhost:8080/ingredients'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getIngredientsForRecipe(recipeId: number): Observable<Ingredient[]> {
+    return this.httpClient.get<Ingredient[]>(`${this.url}/${recipeId}`)
+  }
+}
