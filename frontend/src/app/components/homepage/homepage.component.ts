e88b21ecb5c072df93f1dcee612155b652a7ae2f
matejapetkovska <matejapetkovska2@gmail.com>
2024-01-07 21:05:42 +0100

    Animations on home page

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef, Renderer2, HostListener, AfterViewInit} from '@angular/core';
 import {RecipeService} from "../../services/recipe.service";
 import {Recipe} from "../../interfaces/recipe";
 import {Router} from "@angular/router";
@@ -8,11 +8,16 @@ import {Router} from "@angular/router";
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit, AfterViewInit {
 
   recipes: Recipe[] | undefined
 
-  constructor(private recipeService: RecipeService, private router: Router) {
+  constructor(private recipeService: RecipeService, private router: Router, private el: ElementRef, private renderer: Renderer2) {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.checkCardsVisibility();
   }
 
   ngOnInit(): void {
@@ -29,7 +34,35 @@ export class HomepageComponent implements OnInit {
     })
   }
 
+  checkCardsVisibility() {
+    const cards = this.el.nativeElement.querySelectorAll('.cards-container');
+    for (let i = 0; i < cards.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = cards[i].getBoundingClientRect().top;
+      const elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        cards[i].classList.add("active", "animate__animated", "animate__fadeInUp");
+      } else {
+        cards[i].classList.remove("active", "animate__animated", "animate__fadeInUp");
+      }
+
+    }
+  }
+
+
   onClick(){
     this.router.navigateByUrl("/recipes")
+  }
+
+  ngAfterViewInit(): void {
+    const logo = this.el.nativeElement.querySelector('.logo');
+    logo.addEventListener('animationend', () => {
+      this.startButtonAnimation();
+    });
+  }
+
+  startButtonAnimation() {
+    const button = this.el.nativeElement.querySelector('.text-over-image > button');
+    this.renderer.addClass(button, 'fadeIn');
   }
 }
