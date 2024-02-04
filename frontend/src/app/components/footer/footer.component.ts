f1b47f089ab2f8c70de1e781c038a165b333f030
matejapetkovska <matejapetkovska2@gmail.com>
2024-02-04 21:23:38 +0100

    Final version

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {User} from "../../interfaces/user";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  user: User | undefined
+
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.getUserFromToken()
+  }
+
+  getUserFromToken() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.log('Token is missing.');
+      return;
+    }
+    this.userService.getUserFromToken(token).subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+      error: () => {
+        console.log("Error in getting user from token");
+      }
+    });
+  }
 }
